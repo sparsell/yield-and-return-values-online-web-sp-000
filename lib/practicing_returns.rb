f917eb4c3c9d2ed009c099cf29953e5045b1f36a
Sonja Parsell <hello@sparsell.com>
2020-03-30 13:27:17 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,14 +1,5 @@
 require 'pry'
 
-
-# def hello(array)
-#   i = 0
-#   while i < array.length
-#     binding.pry
-#     yield(array[i])
-#     i += 1
-#   end
-# end
 # 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
